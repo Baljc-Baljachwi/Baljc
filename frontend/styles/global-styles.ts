40f360acaa35c6c3dce75e8a201d868b402bfa63
한지희 <g2hhh2ee@gmail.com>
2022-04-25 15:50:41 +0900

    Set: styled-components 추가

---
+++ frontend/styles/global-styles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  html, body, #__next {
+    height: 100%;
+  }
+  body { font-family: font-family: 'Noto Sans KR', cursive, sans-serif; }
+    .normal		{ font-weight: 400 }
+    .bold		{ font-weight: 700 }
+    .bolder		{ font-weight: 800 }
+    .light		{ font-weight: 300 }
+  a { cursor: pointer; text-decoration: none; }
+`;
