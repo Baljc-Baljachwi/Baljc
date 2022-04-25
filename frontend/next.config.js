b5a88f3c4f133ca9ccffc1ef3c25ba4443b86cf5
이성재 <xpsxm468@naver.com>
2022-04-25 20:47:49 +0900

    chore: kakao login client id, redirect uri env 세팅

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,16 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID;
+const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
+
 module.exports = withPWA({
   pwa: {
     dest: "public",
     runtimeCaching,
+  },
+  env: {
+    KAKAO_CLIENT_ID,
+    KAKAO_REDIRECT_URI,
   },
 });
