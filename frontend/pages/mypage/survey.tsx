cbfc991bda039dc7e4ec90da22bcd1dc0f3c112e
이성재 <xpsxm468@naver.com>
2022-04-26 11:26:08 +0900

    Feat: 설문 조사 페이지만 생성

---
+++ frontend/pages/mypage/survey.tsx
@@ -0,0 +1,10 @@
+import Header from "../../components/common/Header";
+
+export default function survey() {
+  return (
+    <>
+      <Header label="설문 조사"></Header>
+      <h1>설문 조사</h1>
+    </>
+  );
+}
