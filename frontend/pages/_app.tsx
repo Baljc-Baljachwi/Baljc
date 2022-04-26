92fa3d5fe1cb5a5432c414a23f2f3c26d4b908c6
한지희 <g2hhh2ee@gmail.com>
2022-04-26 23:00:24 +0900

    Style: _app.tsx 수정

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -17,21 +17,38 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
-            rel="stylesheet"
-          />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>발자취</title>
           <meta
             name="viewport"
             content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
           />
-          <meta name="description" content="Description" />
-          <meta name="keywords" content="Keywords" />
-          <title>발자취</title>
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <meta name="apple-mobile-web-app-title" content="발자취" />
+          <meta name="application-name" content="발자취" />
+          <meta name="description" content="슬기로운 자취생활 길잡이" />
+          {/* <meta
+            name="theme-color"
+            content="#f4f4f5"
+            media="(prefers-color-scheme: light)"
+          />
+          <meta
+            name="theme-color"
+            content="#18181b"
+            media="(prefers-color-scheme: dark)"
+          /> */}
+          {/* <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover"
+          /> */}
           <link
             rel="apple-touch-icon"
             sizes="57x57"
@@ -101,7 +118,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             sizes="16x16"
             href="/icons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/manifest.json" />
           <link
             href="/icons/favicon-16x16.png"
             rel="icon"
@@ -114,87 +130,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             type="image/png"
             sizes="32x32"
           />
-          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-          <meta name="theme-color" content="#317EFB" />
-          <meta name="application-name" content="PWA App" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-          {/* <meta name="발자취" content="PWA App" /> */}
-          {/* <meta name="description" content="슬기로운 자취생활 길잡이" /> */}
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta
-            name="msapplication-config"
-            content="/icons/browserconfig.xml"
-          />
-          <meta name="msapplication-TileColor" content="#2B5797" />
-          <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
-
-          {/* <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/icons/touch-icon-ipad.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/icons/touch-icon-iphone-retina.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="167x167"
-            href="/icons/touch-icon-ipad-retina.png"
-          />
-
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/icons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/icons/favicon-16x16.png"
-          />
           <link rel="manifest" href="/manifest.json" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            rel="mask-icon"
-            href="/icons/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
-          <link rel="shortcut icon" href="/favicon.ico" /> */}
-          {/* <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-          /> */}
-
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
-          <meta
-            name="twitter:description"
-            content="Best PWA App in the world"
-          />
-          <meta
-            name="twitter:image"
-            content="https://yourdomain.com/icons/android-chrome-192x192.png"
-          />
-          <meta name="twitter:creator" content="@DavidWShadow" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
-          <meta
-            property="og:image"
-            content="https://yourdomain.com/icons/apple-touch-icon.png"
           />
         </Head>
         <Layout>
