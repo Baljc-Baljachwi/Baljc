332a067eeb3984a019e577ecb59307aca1014246
박주미 <zummy@kakao.com>
2022-04-26 16:00:00 +0900

    Feat: 할 일 페이지 - 오늘의 일과 레이아웃 추가

---
+++ frontend/components/work/routine/index.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import Icon from "../../common/Icon";
+
+const RoutineDiv = styled.div`
+  background: #f4f4f4;
+  border-radius: 1rem;
+  width: 100%;
+  padding: 1.5rem 2rem;
+  filter: drop-shadow(0 0.2rem 0.4rem rgba(0, 0, 0, 0.25));
+`;
+
+const RoutineHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const RoutineList = styled.section`
+  margin-top: 1rem;
+  font-size: 1.6rem;
+`;
+
+const RoutineListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 0.2rem;
+`;
+
+export default function Routine() {
+  return (
+    <>
+      <RoutineDiv>
+        <RoutineHeader>
+          <Icon mode="fas" icon="thumbtack" color="#EB3232" size="1.6rem" />
+          <h1>오늘의 일과</h1>
+        </RoutineHeader>
+        <RoutineList>
+          <RoutineListItem>
+            <Icon mode="fas" icon="circle" color="#8CBFF2" size="1rem" />
+            <p>분리수거</p>
+          </RoutineListItem>
+          <RoutineListItem>
+            <Icon mode="fas" icon="circle" color="#8CBFF2" size="1rem" />
+            <p>헬스가기</p>
+          </RoutineListItem>
+        </RoutineList>
+      </RoutineDiv>
+    </>
+  );
+}
--- frontend/pages/work.tsx
+++ frontend/pages/work.tsx
@@ -1,7 +1,23 @@
-export default function Work() {
+import Header from "../components/common/Header";
+import Weekly from "../components/work/Weekly";
+import Routine from "../components/work/routine/index";
+import Todo from "../components/work/todolist/index";
+
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  padding: 2rem;
+`;
+
+export default function Home() {
   return (
-    <div>
-      <h1>Work</h1>
-    </div>
+    <>
+      <Header label="할 일" />
+      <StyledDiv>
+        <Weekly></Weekly>
+        <Routine></Routine>
+        <Todo></Todo>
+      </StyledDiv>
+    </>
   );
 }
