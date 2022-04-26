45bdd39cbd4a9af17d27bdc2460267e1d021a620
한지희 <g2hhh2ee@gmail.com>
2022-04-26 17:44:28 +0900

    Style: 전역 colors 파일 추가

---
+++ frontend/styles/colors/colors.ts
@@ -0,0 +1,25 @@
+const colors = {
+  navy100: "#2E437A",
+
+  lightGray50: "#F4F4F4",
+  lightGray75: "#696969",
+  lightGray100: "#A3A3A3",
+
+  charcoal50: "#474747",
+  charcoal100: "#3D3D3D",
+
+  yellow100: "##FFD469",
+
+  //
+
+  white: "#FFFFFF",
+  black: "#1B1F24",
+
+  error: "#F04438",
+  errorOpacity100: "#F8736A33",
+  errorOpacity200: "#F04438CF",
+
+  transparent: "#00000000",
+};
+
+export default colors;
