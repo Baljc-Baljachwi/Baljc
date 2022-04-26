15ea6271dc43df04fbda9dffc4d5229ae09898ca
한지희 <g2hhh2ee@gmail.com>
2022-04-26 22:58:40 +0900

    Style: colors 속성 추가

--- frontend/styles/colors/colors.ts
+++ frontend/styles/colors/colors.ts
@@ -1,4 +1,12 @@
 const colors = {
+  white: "#FFFFFF",
+  black: "#1B1F24",
+  error: "#F04438",
+  errorOpacity100: "#F8736A33",
+  errorOpacity200: "#F04438CF",
+
+  transparent: "#00000000",
+
   navy100: "#2E437A",
 
   lightGray50: "#F4F4F4",
@@ -10,16 +18,27 @@ const colors = {
 
   yellow100: "##FFD469",
 
-  //
+  blue50: "#A5C1F7",
+  blue150: "#4A83EF",
+  blue100: "#4A83EF",
+  blue200: "#3669CD",
 
-  white: "#FFFFFF",
-  black: "#1B1F24",
+  blueOpacity50: "#4A83EF22",
+  blueOpacity100: "#4A83EF33",
+  blueOpacity200: "#4A83EF77",
+  blueOpacity300: "#4A83EF9e",
 
-  error: "#F04438",
-  errorOpacity100: "#F8736A33",
-  errorOpacity200: "#F04438CF",
-
-  transparent: "#00000000",
+  gray25: "#F8F9FA",
+  gray50: "#F1F3F5",
+  gray100: "#E9ECEF",
+  gray200: "#DEE2E6",
+  gray300: "#CED4DA",
+  gray400: "#ADB5BD",
+  gray500: "#868E96",
+  gray600: "#4A5056",
+  gray700: "#3D4248",
+  gray800: "#31363B",
+  gray900: "#292D32",
 };
 
 export default colors;
