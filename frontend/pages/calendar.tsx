d6d09b101148205a3439e9414fecfe58a4f24297
김윤하 <beupthere@gmail.com>
2022-04-27 13:56:19 +0900

    Feat: Daily 컨텐츠 - 가계부 Board 추가

--- frontend/pages/calendar.tsx
+++ frontend/pages/calendar.tsx
@@ -1,9 +1,14 @@
+import styled from "styled-components";
 import Monthly from "../components/calendar/Monthly";
+
+const Container = styled.div`
+  padding-bottom: 7rem;
+`;
 
 export default function Calendar() {
   return (
-    <div>
+    <Container>
       <Monthly />
-    </div>
+    </Container>
   );
 }
