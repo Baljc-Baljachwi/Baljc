5791d46b3c11c4558a19277ce868b922a88042b2
이성재 <xpsxm468@naver.com>
2022-04-27 15:28:26 +0900

    Feat: 가계부 추가 페이지 레이아웃 UI

--- frontend/components/finance/form/CostForm.tsx
+++ frontend/components/finance/form/CostForm.tsx
@@ -1,11 +1,15 @@
+import React, { useState, memo } from "react";
 import styled from "styled-components";
 
 import Icon from "../../common/Icon";
+import ButtonTogglePaymentMethod from "./ButtonTogglePaymentMethod";
+import ButtonTogglePeriodType from "./ButtonTogglePeriodType";
+import ButtonDaySelect from "./ButtonDaySelect";
 
-const InputContainer = styled.div`
+const InputContainer = styled.div<{ isPrice?: boolean }>`
   width: 100%;
   border-bottom: 1px solid #cccccc;
-  margin: 1.6rem 0 4rem 0;
+  margin: ${(props) => (props.isPrice ? "1.6rem 0 0 0" : "1.6rem 0 4rem 0")};
   color: #cccccc;
   :focus-within {
     border-bottom: 1px solid #3d3d3d;
@@ -48,52 +52,221 @@ const StyledLabel = styled.label`
   margin-top: 1.6rem;
 `;
 
+const CheckboxContainer = styled.div`
+  display: flex;
+  justify-content: end;
+  padding-top: 1rem;
+  gap: 0.5rem;
+  margin-bottom: 3rem;
+`;
+
 const StyledCheckBox = styled.input`
-  width: 1.6rem;
-  :checked + label {
-    content: "\f14a";
-  }
+  display: none;
 `;
 
 const CheckLabel = styled.label`
-  display: inline-block;
-  width: 1.6rem;
-  height: 1.6rem;
-  border: 2px solid #cccccc;
-  border-radius: 3px;
-
   cursor: pointer;
 `;
 
+interface ICostForm {
+  title: string; // 1 ~ 18자
+  price: number;
+  memo: string; // 100자 이하
+  paymentMethod: "M" | "C" | "E"; // M: 현금, C: 카드, E: 기타
+  fixed: boolean;
+
+  // fixed true
+  periodType: "M" | "W" | "D" | null; // M: 매월, W: 매주
+  monthlyPeriod: number | null; // 1 ~ 31 사이.
+  weeklyPeriod: number | null; // 비트마스킹 이용, 일 목 => (1000100) = 68.
+
+  // fixed false
+  date: string | null;
+  time: string | null;
+}
+
 export default function CostForm() {
+  const [costForm, setCostForm] = useState<ICostForm>({
+    paymentMethod: "C",
+  } as ICostForm);
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    if (target.type !== "checkbox") {
+      setCostForm((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    } else if (target.checked) {
+      // 고정 지출 체크 시 불필요 데이터 null 처리
+      setCostForm((prev) => ({
+        ...prev,
+        [name]: target.checked,
+        periodType: "M",
+        date: null,
+      }));
+    } else {
+      // 고정 지출 체크 해제 시 불필요 데이터 null 처리
+      setCostForm((prev) => ({
+        ...prev,
+        [name]: target.checked,
+        periodType: null,
+        monthlyPeriod: null,
+        weeklyPeriod: null,
+      }));
+    }
+  }
+
+  function handleTogglePaymentMethod(value: "M" | "C" | "E") {
+    setCostForm((prev) => ({
+      ...prev,
+      paymentMethod: value,
+    }));
+  }
+
+  function handleTogglePeriodType(value: "M" | "W" | "D" | null) {
+    // 바뀔 때마다 필요없는 데이터 null로
+    switch (value) {
+      case "M":
+        setCostForm((prev) => ({
+          ...prev,
+          periodType: value,
+          weeklyPeriod: null,
+          date: null,
+        }));
+        break;
+      case "W":
+        setCostForm((prev) => ({
+          ...prev,
+          periodType: value,
+          monthlyPeriod: null,
+          date: null,
+        }));
+        break;
+      case "D":
+        setCostForm((prev) => ({
+          ...prev,
+          periodType: value,
+          monthlyPeriod: null,
+          weeklyPeriod: null,
+          date: null,
+        }));
+        break;
+      case null:
+        setCostForm((prev) => ({
+          ...prev,
+          periodType: value,
+          monthlyPeriod: null,
+          weeklyPeriod: null,
+        }));
+        break;
+    }
+  }
+
+  function handleWeeklyDayUpdate(value: number) {
+    let newValue = 0;
+    if (!costForm.weeklyPeriod) {
+      newValue = 1 << value;
+    } else if (costForm.weeklyPeriod & (1 << value)) {
+      // 이미 선택된 경우
+      newValue = costForm.weeklyPeriod - (1 << value);
+    } else {
+      // 새로 선택한 경우
+      newValue = costForm.weeklyPeriod + (1 << value);
+    }
+    setCostForm((prev) => ({
+      ...prev,
+      weeklyPeriod: newValue,
+    }));
+  }
+
   return (
     <>
       <StyledLabel>제목</StyledLabel>
       <InputContainer>
-        <StyledInput />
+        <StyledInput
+          name="title"
+          value={costForm.title || ""}
+          onChange={handleInputChange}
+        />
       </InputContainer>
 
       <StyledLabel>금액</StyledLabel>
-      <InputContainer>
-        <StyledInput type="number" placeholder="0" />
-        <InputUnit hasValue={false}>원</InputUnit>
+      <InputContainer isPrice={true}>
+        <StyledInput
+          type="number"
+          placeholder="0"
+          name="price"
+          value={costForm.price || ""}
+          onChange={handleInputChange}
+        />
+        <InputUnit hasValue={costForm.price > 0}>원</InputUnit>
       </InputContainer>
-      <StyledCheckBox type="checkbox" id="fixed" />
-      <CheckLabel htmlFor="fixed">
-        {/* <Icon
-          mode="fas"
-          icon="square-check"
-          color="#ffd469"
-          size="1.6rem"
-        ></Icon>
-        <Icon mode="far" icon="square" color="#ffd469" size="1.6rem"></Icon> */}
-      </CheckLabel>
-      <label htmlFor="fixed">고정지출</label>
+
+      <CheckboxContainer>
+        <StyledCheckBox
+          type="checkbox"
+          id="fixed"
+          name="fixed"
+          checked={costForm.fixed || false}
+          onChange={handleInputChange}
+        />
+        <CheckLabel htmlFor="fixed">
+          {costForm.fixed ? (
+            <Icon
+              mode="fas"
+              icon="square-check"
+              color="#ffd469"
+              size="1.6rem"
+            />
+          ) : (
+            <Icon mode="far" icon="square" color="#ffd469" size="1.6rem" />
+          )}
+        </CheckLabel>
+        <label htmlFor="fixed">고정지출</label>
+      </CheckboxContainer>
 
       <StyledLabel>날짜</StyledLabel>
-      <InputContainer>
-        <StyledInput type="date" />
-      </InputContainer>
+      {costForm.periodType === "M" ? (
+        <>
+          <ButtonTogglePeriodType
+            selectedPeriodType={costForm.periodType || "M"}
+            handleToggleButton={handleTogglePeriodType}
+          />
+          <InputContainer>
+            <StyledInput
+              type="number"
+              name="monthlyPeriod"
+              value={costForm.monthlyPeriod || 0}
+              onChange={handleInputChange}
+            />
+            <InputUnit hasValue={!!costForm.monthlyPeriod}>일마다</InputUnit>
+          </InputContainer>
+        </>
+      ) : costForm.periodType === "W" ? (
+        <>
+          <ButtonTogglePeriodType
+            selectedPeriodType={costForm.periodType}
+            handleToggleButton={handleTogglePeriodType}
+          />
+          <ButtonDaySelect
+            selectedDays={costForm.weeklyPeriod}
+            handleWeeklyDayUpdate={handleWeeklyDayUpdate}
+          />
+        </>
+      ) : costForm.periodType === "D" ? (
+        <ButtonTogglePeriodType
+          selectedPeriodType={costForm.periodType}
+          handleToggleButton={handleTogglePeriodType}
+        />
+      ) : (
+        <InputContainer>
+          <StyledInput type="date" />
+        </InputContainer>
+      )}
 
       <StyledLabel>시각</StyledLabel>
       <InputContainer>
@@ -103,10 +276,20 @@ export default function CostForm() {
       <StyledLabel>카테고리</StyledLabel>
 
       <StyledLabel>결제 수단</StyledLabel>
+      <ButtonTogglePaymentMethod
+        selectedPaymentMethod={costForm.paymentMethod}
+        handleToggleButton={handleTogglePaymentMethod}
+      />
 
       <StyledLabel>메모</StyledLabel>
       <InputContainer>
-        <StyledInput type="text" placeholder="메모 남기기" />
+        <StyledInput
+          type="text"
+          name="memo"
+          placeholder="메모 남기기"
+          value={costForm.memo || ""}
+          onChange={handleInputChange}
+        />
       </InputContainer>
     </>
   );
