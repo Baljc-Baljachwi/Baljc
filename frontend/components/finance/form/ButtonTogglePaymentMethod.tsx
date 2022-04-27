3eea4dc7d458d08963a03b6f1dc48e7124c7b1a5
이성재 <xpsxm468@naver.com>
2022-04-27 15:27:41 +0900

    Feat: 가계부 추가 페이지 결제 수단 선택 컴포넌트

---
+++ frontend/components/finance/form/ButtonTogglePaymentMethod.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 23.5rem;
+  gap: 1rem;
+  padding: 2rem 0;
+`;
+
+const StyledButton = styled.button<{ isSelected: boolean }>`
+  background-color: ${(props) => (props.isSelected ? "#ffd469" : "#f5f6fa")};
+  color: ${(props) => (props.isSelected ? "#3d3d3d" : "#797979")};
+  width: 100%;
+  font-size: 1.4rem;
+  padding: 0.8rem 0;
+  font-family: "Noto Sans KR", sans-serif;
+  font-weight: 700;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+`;
+type TypePaymentMethod = "M" | "C" | "E";
+
+interface ButtonTogglePaymentMethodProps {
+  selectedPaymentMethod: TypePaymentMethod;
+  handleToggleButton: (value: TypePaymentMethod) => void;
+}
+
+export default function ButtonTogglePaymentMethod({
+  selectedPaymentMethod,
+  handleToggleButton,
+}: ButtonTogglePaymentMethodProps) {
+  function handleClickButton(event: any) {
+    handleToggleButton(event.target.value);
+  }
+  return (
+    <ButtonContainer>
+      <StyledButton
+        value="C"
+        isSelected={selectedPaymentMethod === "C"}
+        onClick={handleClickButton}
+      >
+        카드
+      </StyledButton>
+      <StyledButton
+        value="M"
+        isSelected={selectedPaymentMethod === "M"}
+        onClick={handleClickButton}
+      >
+        현금
+      </StyledButton>
+      <StyledButton
+        value="E"
+        isSelected={selectedPaymentMethod === "E"}
+        onClick={handleClickButton}
+      >
+        기타
+      </StyledButton>
+    </ButtonContainer>
+  );
+}
