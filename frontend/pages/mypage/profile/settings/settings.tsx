5306fdd956117fca7a52f323c90a3dda711452cb
한지희 <g2hhh2ee@gmail.com>
2022-04-28 10:50:13 +0900

    Feat: 마이페이지 settings 페이지 추가

---
+++ frontend/pages/mypage/profile/settings/settings.tsx
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import Header from "components/common/Header";
+import ProfileCard from "components/mypage/ProfileCard";
+import ProfileSettingsList from "components/mypage/settings/ProfileSettingsList";
+
+const Container = styled.div`
+  height: 100vh;
+`;
+const PageContainer = styled.main`
+  font-family: "Noto Sans KR", sans-serif;
+
+  background-color: #ffffff;
+  color: #3d3d3d;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const ProfileCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #2e437a;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 0px 0px 40px 40px;
+  height: 30vh;
+  color: #ffffff;
+  /* padding: 0 2rem; */
+`;
+const PageTitle = styled.span`
+  display: flex;
+  font-size: 1.6rem;
+  font-weight: 500;
+  padding: 2rem 0;
+  color: #33487f;
+`;
+const ProfileContentListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* appearance: none; */
+  border: none;
+  border-radius: 1rem;
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+  /* filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.25)); */
+
+  font-size: 1.6rem;
+  padding: 1.6rem 2rem;
+  gap: 2rem;
+`;
+
+const settings = () => {
+  return (
+    <div>
+      <Container>
+        <Header label="마이페이지" icon="pencil"></Header>
+        <PageContainer>
+          <ProfileCardContainer>
+            <ProfileCard />
+          </ProfileCardContainer>
+          <ProfileContentListContainer>
+            <PageTitle>설정</PageTitle>
+            <ProfileSettingsList />
+          </ProfileContentListContainer>
+        </PageContainer>
+      </Container>
+    </div>
+  );
+};
+
+export default settings;
