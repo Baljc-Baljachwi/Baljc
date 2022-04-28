853311e67d018d116c38016fa2877a4763a702a2
박주미 <zummy@kakao.com>
2022-04-28 17:41:26 +0900

    Style: 오늘의 일과 Header 수정

--- frontend/components/work/routine/RoutineDetail.tsx
+++ frontend/components/work/routine/RoutineDetail.tsx
@@ -1,13 +1,39 @@
 import Header from "../../../components/common/Header";
 import RoutineCard from "./RoutineCard";
+import Icon from "../../../components/common/Icon";
 
 import styled from "styled-components";
+import { useState } from "react";
+
+const StyledHeader = styled.header`
+  width: 100%;
+  height: 6.6rem;
+  background-color: #2e437a;
+  font-size: 2rem;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  padding: 0 2rem;
+  justify-content: space-between;
+`;
+
+const BackButton = styled.span`
+  padding-right: 1rem;
+  cursor: pointer;
+`;
+
+const AddorModifyButton = styled.span`
+  /* padding-right: 2rem; */
+  cursor: pointer;
+`;
 
 const RoutineDiv = styled.div`
   // width: 100%;
 `;
 
 export default function RoutineDetail() {
+  const [open, setOpen] = useState(false);
+
   const routineList = [
     {
       id: 1,
@@ -21,9 +47,23 @@ export default function RoutineDetail() {
     },
   ];
 
+  const onClick = () => {
+    setOpen((prev) => !prev);
+  };
+
   return (
     <>
-      <Header label="오늘의 일과" icon="plus" />
+      <StyledHeader>
+        <div>
+          <BackButton>
+            <Icon mode="fas" icon="chevron-left" color="#ffffff" size="16px" />
+          </BackButton>
+          오늘의 일과
+        </div>
+        <AddorModifyButton onClick={onClick}>
+          <Icon mode="fas" icon="plus" color="#ffffff" size="16px" />
+        </AddorModifyButton>
+      </StyledHeader>
       <RoutineDiv>
         {routineList.map((list) => (
           <RoutineCard key={list.id} list={list}></RoutineCard>
