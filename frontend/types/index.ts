46826e9136a5e53dac1f27006668d440933756b5
이성재 <xpsxm468@naver.com>
2022-04-28 13:57:54 +0900

    Refactor: types/index.ts type들 추가

--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -1,14 +1,19 @@
+export type AccountType = "E" | "I"; // E: 지출, I: 수입, ERD는 type이지만 예약어라 문제될까봐 accountType으로 수정
+export type PaymentMethodType = "M" | "C" | "E" | "N"; // M: 현금, C: 카드, E: 기타, N: 없음
+export type PeriodType = "M" | "W" | "D" | "N"; // M : 매월, W: 매주, D: 매일, N: 없음
+export type SalaryType = "M" | "H" | "N"; // M: 월급, H: 시급, N: 없음
+
 export interface IAccountBook {
   accountBookId: number;
-  accountType: "E" | "I"; // E: 지출, I: 수입, ERD는 type이지만 예약어라 문제될까봐 accountType으로 수정
+  accountType: AccountType;
   categoryType: number;
   title: string;
   price: number;
   memo: string | null;
-  paymentMethod: "M" | "C" | "E" | "N"; // M: 현금, C: 카드, E: 기타, N: 없음
+  paymentMethod: PaymentMethodType;
   fixedExpenditureYn: boolean;
   fixedIncomeYn: boolean;
-  periodType: "M" | "W" | "D" | "N"; // M : 매월, W: 매주, D: 매일, N: 없음
+  periodType: PeriodType;
   monthlyPeriod: number | null;
   weeklyPeriod: number | null;
   date: string | null;
@@ -16,7 +21,7 @@ export interface IAccountBook {
 
 export interface ICategory {
   categoryId: number;
-  accountType: "E" | "I";
+  accountType: AccountType;
   name: string;
   imgUrl: string;
 }
@@ -35,7 +40,7 @@ export interface IMember {
   email: string;
   nickname: string;
   profileUrl: string | null;
-  salaryType: "M" | "H" | "N"; // M: 월급, H: 시급, N: 없음
+  salaryType: SalaryType;
   salary: number | null;
   workingHours: number | null;
   budget: number;
