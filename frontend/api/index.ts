d7f9a66bf4152c878c61011c4aa63cee3b52bd98
이성재 <xpsxm468@naver.com>
2022-05-02 21:02:52 +0900

    Fix: axios interceptor 추가

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,56 +1,28 @@
 import axios from "axios";
-
-class LocalStorage {
-  constructor() {}
-
-  static setItem(key: string, item: string) {
-    if (typeof window !== "undefined") {
-      localStorage.setItem(key, item);
-    }
-  }
-
-  static getItem(key: string) {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem(key);
-    }
-    return null;
-  }
-
-  static removeItem(key: string) {
-    if (typeof window !== "undefined") {
-      localStorage.removeItem(key);
-    }
-  }
-}
+import setInterceptors from "./interceptors";
 
 function createBasicInstance() {
-  const tokenItem = LocalStorage.getItem("accessToken");
-  const accessToken = tokenItem ? JSON.parse(tokenItem)["accessToken"] : null;
-
-  console.log("Create basic instance");
   const instance = axios.create({
     baseURL: process.env.BASE_URL,
     headers: {
       "Content-Type": "application/json",
-      Authorization: accessToken,
     },
   });
+
+  setInterceptors(instance);
 
   return instance;
 }
 
 function createFileInstance() {
-  const tokenItem = LocalStorage.getItem("accessToken");
-  const accessToken = tokenItem ? JSON.parse(tokenItem)["accessToken"] : null;
-  console.log("Create basic instance");
-
   const instance = axios.create({
     baseURL: process.env.BASE_URL,
     headers: {
       "Content-Type": "multipart/form-data",
-      Authorization: accessToken,
     },
   });
+
+  setInterceptors(instance);
 
   return instance;
 }
