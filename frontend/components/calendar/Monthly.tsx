aa24d6f0fc3376958e10c22045967994c842cdc1
김윤하 <beupthere@gmail.com>
2022-05-03 21:49:24 +0900

    Feat: 캘린더 - 데일리 가계부 String API 연동

--- frontend/components/calendar/Monthly.tsx
+++ frontend/components/calendar/Monthly.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import dayjs from "dayjs";
 import "dayjs/locale/ko";
@@ -32,22 +32,21 @@ const CalendarWrapper = styled.div`
 
 const MonthlyTotal = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  font-size: 2.5rem;
+  justify-content: end;
+  font-size: 2.4rem;
   padding: 2rem 2rem 0 2rem;
   font-weight: 700;
   .title {
     font-size: 1.5rem;
-
     color: #3d3d3d;
     padding-bottom: 1rem;
   }
   .flex_wrapper {
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 45%;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: space-around;
+    padding-left: 1.5rem;
   }
   .label {
     font-weight: 300;
@@ -62,11 +61,11 @@ const MonthlyTotal = styled.div`
 export default function Monthly() {
   const [date, setDate] = useState(new Date());
   const [mounted, setMounted] = useState(false);
-  const year = Number(dayjs(date).format("YYYY"));
-  const month = Number(dayjs(date).format("MM"));
+  const year = dayjs(date).format("YYYY");
+  const month = dayjs(date).format("M");
+  const day = dayjs(date).format("D");
   const [expenditure, setExpenditure] = useState("");
   const [income, setIncome] = useState("");
-  const ref = React.useRef(null);
 
   // 기록 있는 날짜
   const [mark, setMark] = useState([]);
@@ -79,8 +78,7 @@ export default function Monthly() {
   const amount = Object.entries(mark);
   // [['2022-05-1', {E: 3500, I: 1000}], ['2022-05-2', {E: 10000}]]
 
-  const day = dayjs(date).format("D일 dddd");
-
+  const dayYoil = dayjs(date).format("D일 dddd");
   dayjs.locale("ko");
 
   useEffect(() => {
@@ -115,15 +113,14 @@ export default function Monthly() {
         <StyledHeader>
           <div className="title">이번 달 요약</div>
         </StyledHeader>
-
         <MonthlyTotal>
           <div className="flex_wrapper">
             <div className="label">지출</div>
-            <div className="e_content">-{expenditure}원</div>
+            <div className="label">수입</div>
           </div>
           <div className="flex_wrapper">
-            <div className="label">수입</div>
-            <div className="i_cotent">{income}원</div>
+            <div className="e_content">-{expenditure.toLocaleString()}원</div>
+            <div className="i_cotent">{income.toLocaleString()}원</div>
           </div>
         </MonthlyTotal>
 
@@ -134,7 +131,6 @@ export default function Monthly() {
             calendarType="US" // 일요일 시작
             formatDay={(locale, date) => dayjs(date).format("D")} // 날짜 표기 방식 변경
             formatMonthYear={(locale, date) => dayjs(date).format("M월")} // 월 표기 방식 변경
-            inputRef={ref}
             showNeighboringMonth={false} // 이전, 이후 달 날짜 보이지 않도록
             next2Label={null} // 연 단위 이동 삭제
             prev2Label={null}
@@ -164,7 +160,7 @@ export default function Monthly() {
             }
           />
         </CalendarWrapper>
-        <Daily day={day} />
+        <Daily dayYoil={dayYoil} day={day} year={year} month={month} />
       </Container>
     </>
   );
--- frontend/components/calendar/daily/index.tsx
+++ frontend/components/calendar/daily/index.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import { useState } from "react";
+
 import FinanceBoard from "./FinanceBoard";
 import RoutineBoard from "./RoutineBoard";
 import ToDoBoard from "./ToDoBoard";
+import { getDailyCalendar } from "api/calendar";
+import { useEffect } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -15,14 +19,25 @@ const Title = styled.div`
 `;
 
 interface DailyProps {
+  year: string;
+  month: string;
   day: string;
+  dayYoil: string;
 }
 
-export default function Daily({ day }: DailyProps) {
+export default function Daily({ dayYoil, day, month, year }: DailyProps) {
+  const [result, setResult] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    getDailyCalendar({ year: year, month: month, day: day }).then((res) => {
+      setResult(res.data.data);
+    });
+  }, [day, month, year]);
+
   return (
     <Container>
-      <Title>{day}</Title>
-      <FinanceBoard />
+      <Title>{dayYoil}</Title>
+      <FinanceBoard item={result} />
       <RoutineBoard />
       <ToDoBoard />
     </Container>
