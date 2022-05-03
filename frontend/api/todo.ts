b63f01058bfe742330e9293514c254ee534df7bc
박주미 <zummy@kakao.com>
2022-05-04 00:59:30 +0900

    Feat: todo 완료 여부 수정 API 연동

--- frontend/api/todo.ts
+++ frontend/api/todo.ts
@@ -2,12 +2,19 @@ import { api } from "api";
 
 // todo 조회 API
 export const getTodos = async (date: string) => {
-  console.log(date);
+  // console.log(date);
   return await api.get(`/api/todos?date=${date}`);
 };
 
 // todo 생성 API
 export const postTodos = async (data: object) => {
-  console.log(data);
+  // console.log(data);
   return await api.post(`/api/todos`, data);
+};
+
+// todo 완료 여부 API
+export const completedTodo = async (todoId: string, completedYn: object) => {
+  // console.log(todoId);
+  // console.log(completedYn);
+  return await api.patch(`/api/todos/${todoId}/done`, completedYn);
 };
