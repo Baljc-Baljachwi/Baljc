8a2d4057875526066fded2f4d70a00d1f5f3425c
한지희 <g2hhh2ee@gmail.com>
2022-05-05 16:24:41 +0900

    Feat: 예산 조회, 카테고리 별 지출 분석 조회 완성

--- frontend/pages/mypage/analysis.tsx
+++ frontend/pages/mypage/analysis.tsx
@@ -35,8 +35,6 @@ interface AnalysisProps {
 const Analysis = () => {
   // const [budgets, setBudgets] = useState(0);
   const [date, setDate] = useState(new Date());
-  // const [month, setMonth] = useState(Number(dayjs(date).format("M")));
-  // const [year, setYear] = useState(Number(dayjs(date).format("YYYY")));
   const [remainingBudget, setRemainingBudget] = useState(0);
   const [dailyExpenditure, setDailyExpenditure] = useState(0);
   const [estimatedExpenditure, setEstimatedExpenditure] = useState(0);
@@ -45,8 +43,13 @@ const Analysis = () => {
   const month = Number(dayjs(date).format("M"));
   const dateForm = dayjs(date).format("YYYY-MM-DD");
 
+  // const categoryName = Object.keys(categories).map((idx:any)=> data1.labels[idx]);
   const categoryName = Object.keys(categories);
   const categoryValue = Object.values(categories);
+  const categoryContents = Object.entries(categories).map((entrie, idx) => {
+    return console.log(entrie, idx);
+  });
+  console.log(categoryContents);
   console.log(categoryName);
   console.log(categoryValue);
   useEffect(() => {
@@ -75,24 +78,31 @@ const Analysis = () => {
       });
   }, [month, year]);
   // doughnut chart data set
+  // const getKeys = categoryName.map((entrie, idx) => {
+  //   return data1.labels[idx];
+  // });
+
   const data1 = {
-    labels: [
-      "문화/여가",
-      "육아/반려",
-      "교육/학습",
-      "경조사비",
-      "미분류",
-      "의료/건강",
-      "교통비",
-      "쇼핑",
-      "식비",
-      "주거/통신",
-    ],
+    // labels: [
+    //   categoryName,
+    //   "문화/여가",
+    //   "육아/반려",
+    //   "교육/학습",
+    //   "경조사비",
+    //   "미분류",
+    //   "의료/건강",
+    //   "교통비",
+    //   "쇼핑",
+    //   "식비",
+    //   "주거/통신",
+    // ],
+    labels: categoryName,
     datasets: [
       {
-        data: [
-          30000, 15000, 35000, 20000, 20000, 45000, 15000, 15000, 15000, 330000,
-        ],
+        // data: [
+        //   30000, 15000, 35000, 20000, 20000, 45000, 15000, 15000, 15000, 330000,
+        // ],
+        data: categoryValue,
         backgroundColor: [
           "#ff9aa2",
           "#abbeec",
@@ -118,41 +128,7 @@ const Analysis = () => {
       },
     ],
   };
-  // const getBudgetList = useCallback(() => {
-  //   console.log(date);
-  //   getBudget(date)
-  //     .then((res) => {
-  //       // console.log(res.data.data);
-  //       // setBudgets(res.data.data);
-  //       console.log(res.data);
-  //       console.log("예산 조회 성공! 🤸‍♀️🔥");
-  //       setRemainingBudget(res.data.data.remainingBudget);
-  //       setDailyExpenditure(res.data.data.dailyExpenditure);
-  //       setEstimatedExpenditure(res.data.data.estimatedExpenditure);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.response);
-  //       console.log("😥🙀 예산 조회 실팩ㄱ");
-  //     });
-  // }, [date, setBudgets]);
-  // useEffect(() => {
-  //   getBudgetList();
-  // }, [getBudgetList]);
 
-  // useEffect(() => {
-  //   getBudget()
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       console.log("알림 조회 성공! 🤸‍♀️🔥");
-  //       setRemainingBudget(res.data.data.remainingBudget);
-  //       setDailyExpenditure(res.data.data.dailyExpenditure);
-  //       setEstimatedExpenditure(res.data.data.estimatedExpenditure);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.response);
-  //       console.log("😥🙀 알림 조회 실팩ㄱ");
-  //     });
-  // }, []);
   return (
     <>
       <Container>
