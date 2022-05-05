0445c9f9b10e07d63e117f12bcd9a2220bd46916
김도현 <kdh494849@gmail.com>
2022-05-05 17:06:10 +0900

    Feat: 로그인/로그아웃 시 FCM 토큰 처리

--- backend/src/main/java/com/baljc/api/service/MemberService.java
+++ backend/src/main/java/com/baljc/api/service/MemberService.java
@@ -5,10 +5,12 @@ import com.baljc.db.entity.Member;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
-    Member signinByKakao(String code);
+//    Member signinByKakao(String code);
+    Member signinByKakao(String code, String fcmToken);
     MemberDto.SigninInfo authenticateMember(Member member);
     Member getMemberByAuthentication();
     MemberDto.Response getMemberInfoByAuthentication();
     void updateMember(MemberDto.RegisterRequest registerRequest, MultipartFile multipartFile);
     void deleteMember();
+    void signoutMember();
 }
