0faf54bbc78db0822fac8d04a2a2237116ee1cfc
이성재 <xpsxm468@naver.com>
2022-05-06 15:39:20 +0900

    Fix: 일과 페이지 새로고침 버그 수정

--- frontend/pages/work/routine/index.tsx
+++ frontend/pages/work/routine/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import RoutineDetail from "../../../components/work/routine/RoutineDetail";
 
 export default function RoutineHome() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+  if (!ready) {
+    return null;
+  }
   return (
     <>
       <RoutineDetail></RoutineDetail>
