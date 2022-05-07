75103095986e1bcffeba11fa7f86c3adf7750ca8
김윤하 <beupthere@gmail.com>
2022-05-07 21:07:17 +0900

    Chore: 커뮤니티 폴더 경로 변경 및 새로고침 설정 추가

--- frontend/pages/community/index.tsx
+++ frontend/pages/community/index.tsx
@@ -1,13 +1,22 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 import Header from "components/common/Header";
-import CommunityList from "../components/community/CommunityList";
+import CommunityList from "../../components/community/CommunityList";
 
 const Container = styled.div`
   padding-bottom: 7rem;
 `;
 
 export default function Community() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+  if (!ready) {
+    return null;
+  }
   return (
     <div>
       <Header label="커뮤니티" icon="plus" />
