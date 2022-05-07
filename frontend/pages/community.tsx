2d6cb09f52233d993a4421572c43652b958c20d9
김윤하 <beupthere@gmail.com>
2022-05-07 20:58:30 +0900

    Feat: 커뮤니티 게시글 목록 UI

--- frontend/pages/community.tsx
+++ frontend/pages/community.tsx
@@ -1,7 +1,19 @@
+import styled from "styled-components";
+
+import Header from "components/common/Header";
+import CommunityList from "../components/community/CommunityList";
+
+const Container = styled.div`
+  padding-bottom: 7rem;
+`;
+
 export default function Community() {
   return (
     <div>
-      <h1>Community</h1>
+      <Header label="커뮤니티" icon="plus" />
+      <Container>
+        <CommunityList />
+      </Container>
     </div>
   );
 }
