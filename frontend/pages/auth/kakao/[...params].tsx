968e4c79167eb8493e809cb6d48a76dde420f7f2
이성재 <xpsxm468@naver.com>
2022-05-09 11:35:38 +0900

    Feat: 카카오 로그인 params에 FCM 토큰 추가

--- frontend/pages/auth/kakao/[...params].tsx
+++ frontend/pages/auth/kakao/[...params].tsx
@@ -4,24 +4,53 @@ import { useRecoilState } from "recoil";
 
 import { accessTokenState } from "atoms/atoms";
 import { kakaoLogin } from "../../../api/member";
+import { getToken } from "utils/FirebaseInit";
 
 export default function KakaoAuth() {
   const [_, setAccessToken] = useRecoilState(accessTokenState);
   const router = useRouter();
   const { code } = router.query;
+
+  const firebaseMessageToken = async () => {
+    try {
+      let token = await getToken();
+      return token;
+    } catch (error) {
+      console.error(error);
+      return "";
+    }
+  };
+
   useEffect(() => {
     if (!code) return;
-    kakaoLogin(code as string).then((res) => {
-      console.log(res.data);
-      if (res.data.code === 1000) {
-        const accessToken = res.headers.authorization;
-        console.log(`accessToken : ${accessToken}`);
-        setAccessToken(accessToken);
-        console.log(res.data.data);
 
-        router.push(res.data.data.surveyedYn ? "/calendar" : "/mypage/survey");
-      }
-    });
+    firebaseMessageToken()
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log("FCM token: ", currentToken);
+          return currentToken;
+        } else {
+          console.log("No FCM Token");
+          return "";
+        }
+      })
+      .then((currentToken) => {
+        kakaoLogin(code as string, currentToken).then((res) => {
+          console.log(res.data);
+          if (res.data.code === 1000) {
+            const accessToken = res.headers.authorization;
+            console.log(`accessToken : ${accessToken}`);
+            setAccessToken(accessToken);
+            console.log(res.data.data);
+
+            router.push(
+              res.data.data.surveyedYn ? "/calendar" : "/mypage/survey"
+            );
+          }
+        });
+      })
+      .catch((err) => console.error(err));
   }, [code, router, setAccessToken]);
+
   return null;
 }
