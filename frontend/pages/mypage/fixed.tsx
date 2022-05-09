937a89b90de468a4124a35fc483393784d85b61e
한지희 <g2hhh2ee@gmail.com>
2022-05-09 23:16:17 +0900

    Feat: 고정 지출 내역 조회 완성

--- frontend/pages/mypage/fixed.tsx
+++ frontend/pages/mypage/fixed.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { getFixedEList } from "api/mypage";
 
 import styled from "styled-components";
 import dayjs from "dayjs";
@@ -7,13 +8,54 @@ import "dayjs/locale/ko";
 
 import Header from "components/common/Header";
 import FixedList from "components/mypage/fixed/FixedList";
-import FixedItem from "components/mypage/fixed/FixedItem";
+interface IExFixed {
+  accountbookId: string;
+  monthlyPeriod: number;
+  dayOfWeek: string;
+  title: string;
+  price: number;
+  categoryName: string;
+  categoryImgUrl: string;
+  paymentMethod: string;
+}
 
 const Fixed = () => {
   const router = useRouter();
   const [date, setDate] = useState(new Date());
   const [month, setMonth] = useState(Number(dayjs(date).format("M")));
   const [year, setYear] = useState(Number(dayjs(date).format("YYYY")));
+  const [exFixed, setExFixed] = useState<IExFixed[]>([]);
+  const [accountbookId, setAccountbookId] = useState("");
+  const [monthlyPeriod, setMonthlyPeriod] = useState("");
+  const [dayOfWeek, setDayOfWeek] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryImgUrl, setCategoryImgUrl] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const fixedEList = Object.entries(exFixed);
+  // console.log(exFixed);
+  // console.log(fixedEList);
+  useEffect(() => {
+    getFixedEList(year, month)
+      .then((res) => {
+        console.log(res);
+        setExFixed(res.data.data);
+        // setAccountbookId(res.data.data.accountbookId);
+        // setMonthlyPeriod(res.data.data.monthlyPeriod);
+        // setDayOfWeek(res.data.data.dayOfWeek);
+        // setTitle(res.data.data.title);
+        // setPrice(res.data.data.price);
+        // setCategoryName(res.data.data.categoryName);
+        // setCategoryImgUrl(res.data.data.categoryImgUrl);
+        // setPaymentMethod(res.data.data.paymentMethod);
+      })
+      .catch((err) => {
+        console.log("😥🙀 고정 지출 조회 실패");
+        console.log(err.response);
+      });
+  }, []);
+  // console.log(exFixed);
   return (
     <>
       <Header
@@ -23,18 +65,17 @@ const Fixed = () => {
       <HeaderCard>
         <span>이번 달 고정 지출</span>
         <span>
-          <span>총 </span>93,900<span>원</span>
+          <span>총 </span>금액<span>원</span>
         </span>
       </HeaderCard>
       <ContentsContainer>
-        {/* {amount && amount.length > 0 ? (
-          amount.map((item, idx) => <FixedList key={idx} item={item} />)
+        {fixedEList && fixedEList.length > 0 ? (
+          fixedEList.map((item, idx) => <FixedList key={idx} item={item} />)
         ) : (
           <NoContentContainer>
             <p>고정 지출 내역이 없습니다!</p>
-            <p className="small">+ 버튼을 눌러 새로운 내용을 추가해보세요</p>
           </NoContentContainer>
-        )} */}
+        )}
       </ContentsContainer>
     </>
   );
@@ -62,11 +103,10 @@ const HeaderCard = styled.div`
 const ContentsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   flex-wrap: nowrap;
 
   height: 100%;
-  padding: 2 2rem;
+  padding: 2rem;
 
   color: #3d3d3d;
   font-size: 1.6rem;
