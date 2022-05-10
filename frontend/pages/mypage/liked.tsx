87c0a62543719a6652fabe757e6ddf46171186fb
한지희 <g2hhh2ee@gmail.com>
2022-05-10 14:53:45 +0900

    Feat: 좋아요 한 글 목록 페이지 UI

---
+++ frontend/pages/mypage/liked.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+
+import Header from "components/common/Header";
+import CommunityCard from "components/community/CommunityCard";
+
+const Liked = () => {
+  const router = useRouter();
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+  if (!ready) {
+    return null;
+  }
+
+  return (
+    <>
+      <Header label='좋아요한 글 목록'></Header>
+      <Container>
+        <CommunityCard />
+        <CommunityCard />
+        <CommunityCard />
+        <CommunityCard />
+      </Container>
+    </>
+  );
+};
+
+export default Liked;
+Liked.requireAuth = true;
+
+const Container = styled.div`
+  padding-bottom: 7rem;
+`;
