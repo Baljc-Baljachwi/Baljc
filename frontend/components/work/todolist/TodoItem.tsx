1257d30ad94cd7e690386905d521bf290c8a0f47
박주미 <zummy@kakao.com>
2022-05-10 16:26:20 +0900

    Feat: todo 수정 유효성 검사 추가

--- frontend/components/work/todolist/TodoItem.tsx
+++ frontend/components/work/todolist/TodoItem.tsx
@@ -12,7 +12,6 @@ const TodoListItem = styled.div`
   display: flex;
   align-items: center;
   gap: 1.5rem;
-  margin: 1rem 0;
 `;
 
 const TodoItemDiv = styled.div`
@@ -22,20 +21,28 @@ const TodoItemDiv = styled.div`
   justify-content: space-between;
 `;
 
-const TodoText = styled.span<{ isCompleted: string; viewOnly: boolean }>`
+const TodoImage = styled.img`
+  width: 3.5rem;
+  margin-bottom: 1.2rem;
+`;
+
+const TodoText = styled.span<{ viewOnly: boolean }>`
   width: 100%;
   font-size: 1.8rem;
-  text-decoration: ${(props) =>
-    props.isCompleted === "Y" ? "line-through" : ""};
+
   color: ${(props) => (props.viewOnly ? "#ffffff" : "")};
   display: flex;
   justify-content: space-between;
 `;
 
+const TodoNErrorDiv = styled.div``;
+
 const TodoInput = styled.input<{
   isClicked: boolean;
   isEdit: boolean;
   viewOnly: boolean;
+  isError: boolean;
+  isCompleted: string;
 }>`
   width: 90%;
   font-family: "Noto Sans KR";
@@ -43,8 +50,22 @@ const TodoInput = styled.input<{
   color: ${(props) => (props.viewOnly ? "#ffffff" : "#000000")};
   background-color: ${(props) => (props.viewOnly ? "#4d5f8f" : "")};
   border: none;
-  border-bottom: ${(props) => (props.isEdit ? "" : " 1px solid #cccccc")};
+  border-bottom: ${(props) =>
+    props.isEdit
+      ? "none"
+      : props.isError
+      ? "1px solid #ff0000"
+      : "1px solid #cccccc"};
   outline: none;
+  text-decoration: ${(props) =>
+    props.isCompleted === "Y" ? "line-through" : ""};
+`;
+
+const ErrorMessage = styled.p<{ isError: boolean }>`
+  height: 1.3rem;
+  visibility: ${(props) => (props.isError ? "visible" : "hidden")};
+  color: #ff0000;
+  font-size: 1.2rem;
 `;
 
 const IconDiv = styled.div<{ isClicked: boolean }>`
@@ -79,8 +100,11 @@ export default function TodoItem({
   const [todoClicked, setTodoClicked] = useState(false); // 수정 삭제 아이콘을 위한
   const [isClicked, setClicked] = useState(true); // todo 클릭 시, readOnly 변경 위한
   const [contentForm, setContentForm] = useState<contentState>({ content: "" });
+  const [isError, setIsError] = useState(false);
 
   const todoRef = useRef() as React.MutableRefObject<HTMLSpanElement>;
+
+  const validationFunc = () => {};
 
   const todoComplete = (e: any) => {
     if (completedYn === "N") {
@@ -115,7 +139,7 @@ export default function TodoItem({
 
   const onEnter = (e: any) => {
     if (e.key === "Enter") {
-      if (contentForm.content.length !== 0) {
+      if (!isError) {
         editTodos(e.target.id, contentForm)
           .then((res) => {
             console.log(res);
@@ -135,10 +159,20 @@ export default function TodoItem({
   };
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContentForm((prev) => ({
-      ...prev,
-      content: e.target.value,
-    }));
+    if (e.target.value.length > 0) {
+      setIsError(false);
+      setContentForm((prev) => ({
+        ...prev,
+        content: e.target.value,
+      }));
+    } else {
+      setIsError(true);
+      setContentForm((prev) => ({
+        ...prev,
+        content: e.target.value,
+      }));
+    }
+    console.log(isError);
   };
 
   const deleteTodo = () => {
@@ -156,7 +190,7 @@ export default function TodoItem({
   };
 
   const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    if (contentForm.content.length !== 0) {
+    if (!isError) {
       editTodos(e.target.id, contentForm)
         .then((res) => {
           console.log(res);
@@ -171,9 +205,9 @@ export default function TodoItem({
           setClicked(true);
         })
         .catch((err) => console.log(err));
+    } else {
+      document.getElementById(todoId)?.focus();
     }
-    setTodoClicked(false);
-    setClicked(true);
   };
 
   useEffect(() => {
@@ -185,36 +219,39 @@ export default function TodoItem({
     <>
       <TodoListItem>
         {completedYn === "Y" ? (
-          <Image
+          <TodoImage
             src="/assets/img/foot_true.png"
             alt=""
-            width={30}
-            height={30}
             onClick={todoComplete}
           />
         ) : (
-          <Image
+          <TodoImage
             src="/assets/img/foot_false.png"
             alt=""
-            width={30}
-            height={30}
             onClick={todoComplete}
           />
         )}
         <TodoItemDiv>
-          <TodoText isCompleted={completedYn} viewOnly={viewOnly} ref={todoRef}>
-            <TodoInput
-              id={todoId}
-              value={contentForm.content}
-              onChange={onInputChange}
-              isClicked={todoClicked}
-              isEdit={isClicked} // false일 때, input 밑줄 생기게
-              onKeyPress={onEnter}
-              // readOnly={isClicked} // false일 때, 수정 가능
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-              viewOnly={viewOnly}
-            />
+          <TodoText viewOnly={viewOnly} ref={todoRef}>
+            <TodoNErrorDiv>
+              <TodoInput
+                id={todoId}
+                value={contentForm.content}
+                onChange={onInputChange}
+                isCompleted={completedYn}
+                isClicked={todoClicked}
+                isEdit={isClicked} // false일 때, input 밑줄 생기게
+                isError={isError}
+                onKeyPress={onEnter}
+                // readOnly={isClicked} // false일 때, 수정 가능
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                viewOnly={viewOnly}
+              />
+              <ErrorMessage isError={isError}>
+                1자 이상 입력해주세요 !
+              </ErrorMessage>
+            </TodoNErrorDiv>
             <IconDiv isClicked={todoClicked}>
               {/* <Icon
                 mode="fas"
