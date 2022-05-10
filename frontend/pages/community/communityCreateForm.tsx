0476fe499070c4ecc8add2d5494fc0262309e985
이성재 <xpsxm468@naver.com>
2022-05-10 13:54:03 +0900

    Feat: 게시물 작성 이미지 업로드, 미리보기, 삭제 기능 추가, 카테고리 선택 기능 추가

--- frontend/pages/community/communityCreateForm.tsx
+++ frontend/pages/community/communityCreateForm.tsx
@@ -10,6 +10,7 @@ import Icon from "../../components/common/Icon";
 import ButtonBottom from "components/common/ButtonBottom";
 import ButtonTrashCan from "components/common/ButtonTrashCan";
 import ButtonImage from "components/common/ButtonImage";
+import { getBoardsCategories } from "api/community";
 
 const FormContainer = styled.form`
   display: flex;
@@ -23,6 +24,7 @@ const FlexContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  line-height: 1.8rem;
   padding: 0 0 2rem 0;
 `;
 
@@ -38,16 +40,17 @@ const Typography = styled.div<{
 
 const CategoryLabelContainer = styled.div`
   display: flex;
-  width: 23.5rem;
-  gap: 1rem;
+  width: 24rem;
+  gap: 1.4rem;
 `;
 
 const CategoryLabel = styled.label<{ isSelected: boolean }>`
   background-color: ${(props) => (props.isSelected ? "#ffd469" : "#f5f6fa")};
   color: ${(props) => (props.isSelected ? "#3d3d3d" : "#797979")};
   width: 100%;
-  font-size: 1.4rem;
-  padding: 0.8rem 0;
+  font-size: 1.2rem;
+  line-height: 1.6rem;
+  padding: 0.2rem 0;
   font-weight: 700;
   border: none;
   border-radius: 0.5rem;
@@ -67,6 +70,12 @@ const ImageContainer = styled.div`
 
 const ImageWrapper = styled.div`
   position: relative;
+  height: 9rem;
+  width: 9rem;
+  .image {
+    border-radius: 0.5rem;
+    object-fit: contain;
+  }
 `;
 
 const IconContainer = styled.div`
@@ -81,23 +90,22 @@ const IconContainer = styled.div`
 const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  overflow-y: auto;
 `;
 
 const InputDiv = styled.div<{ isError?: boolean }>`
   width: 100%;
+  height: 100%;
+
   font-size: 2rem;
   display: flex;
   gap: 1rem;
-  input[type="month"] {
-    font-size: 1.5rem;
-    + span {
-      font-size: 1.2rem;
-    }
-  }
+  overflow-y: auto;
 `;
 
 const StyledTextarea = styled.textarea`
   width: 100%;
+  height: 100%;
   font-size: 2rem;
   border: none;
   outline: none;
@@ -122,7 +130,25 @@ const ButtonContainer = styled.div`
   gap: 1.6rem;
 `;
 
+const ImageButtonLabel = styled.label`
+  color: #ffffff;
+  display: flex;
+  padding: 1rem 1.2rem;
+  border: 1px solid #8e8e8e;
+  border-radius: 0.8rem;
+  background-color: #ffffff;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+`;
+
 // export type CategoryType = "E" | "C" | "N";
+
+interface IBoardCategory {
+  boardCategoryId: string;
+  imgUrl: string;
+  name: string;
+}
 
 // interface CommunityProps {
 //   category: CategoryType;
@@ -133,6 +159,8 @@ const ButtonContainer = styled.div`
 export default function CommunityCreateForm() {
   const router = useRouter();
 
+  // 새로고침 hydration error 해결
+  const [ready, setReady] = useState(false);
   const initForm = true;
   const type = "E";
 
@@ -149,20 +177,67 @@ export default function CommunityCreateForm() {
       content: "",
     },
   });
+  const category = watch("category");
+  const [imagePreviewList, setImagePreviewList] = useState<string[]>([]);
+  const [imageFileList, setImageFileList] = useState<Blob[]>([]);
+  const [boardCategories, setBoardCategories] = useState<IBoardCategory[]>([]);
 
   const onClickDeleteButton = () => {
     console.log("Delete!");
     // if (!initForm || !initForm.accountbookId) {
     //   return;
     // }
+    console.log(imageFileList);
 
     // 게시글 삭제 API 추가하기
   };
 
-  const onClickImageUpload = () => {
+  function handleImageUpload(event: React.ChangeEvent<HTMLInputElement>) {
     console.log("image upload");
-    // 파일 업로드 API 추가하기
-  };
+    console.log(event.target.files);
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      if (imageFileList.length < 3) {
+        setImageFileList((prev) => [...prev, files[0]]);
+        setImagePreviewList((prev) => [...prev, URL.createObjectURL(files[0])]);
+      } else {
+        console.log("파일은 3개까지만 가능하지롱");
+      }
+    }
+  }
+
+  function onClickImageDeleteButton(index: number) {
+    console.log(index, imagePreviewList[index]);
+    setImagePreviewList((prev) => [
+      ...prev.slice(0, index),
+      ...prev.slice(index + 1, prev.length),
+    ]);
+    setImageFileList((prev) => [
+      ...prev.slice(0, index),
+      ...prev.slice(index + 1, prev.length),
+    ]);
+  }
+
+  function onClickSubmitButton(data: any) {
+    console.log(data);
+    // 게시글 작성 API 추가하기
+
+    const boardInfo = {
+      categoryId: category,
+      content: data.content,
+      place: "",
+    };
+
+    const formData = new FormData();
+    imageFileList.forEach((file) => {
+      formData.append("boardImg", file);
+    });
+    formData.append(
+      "boardInfo",
+      new Blob([JSON.stringify(boardInfo)], { type: "application/json" })
+    );
+    console.log(formData);
+  }
 
   useEffect(() => {
     // 생성 페이지일 때만
@@ -171,55 +246,97 @@ export default function CommunityCreateForm() {
     }
   }, [type, initForm, setValue]);
 
+  useEffect(() => {});
+
+  useEffect(() => {
+    setReady(true);
+    getBoardsCategories().then((res) => {
+      console.log(res.data);
+      if (res.data.code === 1700) {
+        console.log(res.data.data);
+        setBoardCategories(res.data.data);
+      } else {
+        console.log(res.data.message);
+      }
+    });
+  }, []);
+  if (!ready) {
+    return null;
+  }
+
   return (
     <>
       <Header
         label="게시글 작성"
         onClickBackButton={() => router.push("/community")}
       />
-      <FormContainer onSubmit={handleSubmit((data) => console.log(data))}>
-        <div>
-          <FlexContainer>
-            <Typography fs="1.6rem">카테고리</Typography>
-            <CategoryLabelContainer>
-              {[
-                { name: "부탁해요", value: "C" },
-                { name: "같이해요", value: "M" },
-                { name: "동네정보", value: "E" },
-              ].map((item) => (
-                <Fragment key={item.value}>
-                  <CategoryLabel
-                    // isSelected={category === item.value}
-                    isSelected={false}
-                    htmlFor={item.value}
-                  >
-                    {item.name}
-                  </CategoryLabel>
-                  <DisplayNoneInput
-                    {...register("category", {
-                      required: {
-                        value: true,
-                        message: "카테고리를 설정해주세요",
-                      },
-                    })}
-                    type="radio"
-                    value={item.value}
-                    id={item.value}
-                  />
-                </Fragment>
-              ))}
-            </CategoryLabelContainer>
-          </FlexContainer>
-          <ImageContainer>
-            <ImageWrapper>
+      <FormContainer onSubmit={handleSubmit(onClickSubmitButton)}>
+        <FlexContainer>
+          <Typography fs="1.6rem" fw="500">
+            카테고리
+          </Typography>
+          <CategoryLabelContainer>
+            {boardCategories.map((item) => (
+              <Fragment key={item.boardCategoryId}>
+                <CategoryLabel
+                  isSelected={category === item.boardCategoryId}
+                  htmlFor={item.boardCategoryId}
+                >
+                  {item.name}
+                </CategoryLabel>
+                <DisplayNoneInput
+                  {...register("category", {
+                    required: {
+                      value: true,
+                      message: "카테고리를 설정해주세요",
+                    },
+                  })}
+                  type="radio"
+                  value={item.boardCategoryId}
+                  id={item.boardCategoryId}
+                />
+              </Fragment>
+            ))}
+            {/* {[
+              { name: "부탁해요", value: "C" },
+              { name: "같이해요", value: "M" },
+              { name: "동네정보", value: "E" },
+            ].map((item) => (
+              <Fragment key={item.value}>
+                <CategoryLabel
+                  isSelected={category === item.value}
+                  // isSelected={false}
+                  htmlFor={item.value}
+                >
+                  {item.name}
+                </CategoryLabel>
+                <DisplayNoneInput
+                  {...register("category", {
+                    required: {
+                      value: true,
+                      message: "카테고리를 설정해주세요",
+                    },
+                  })}
+                  type="radio"
+                  value={item.value}
+                  id={item.value}
+                />
+              </Fragment>
+            ))} */}
+          </CategoryLabelContainer>
+        </FlexContainer>
+
+        <ImageContainer>
+          {imagePreviewList.map((imageUrl, index) => (
+            <ImageWrapper key={index}>
               <Image
-                src={Avatar}
+                className="image"
+                src={imageUrl}
                 alt=""
-                width={90}
-                height={90}
+                layout="fill"
                 style={{ borderRadius: "5px" }}
               />
-              <IconContainer>
+              <IconContainer onClick={() => onClickImageDeleteButton(index)}>
                 <Icon
                   mode="fas"
                   icon="circle-xmark"
@@ -228,31 +345,47 @@ export default function CommunityCreateForm() {
                 />
               </IconContainer>
             </ImageWrapper>
-          </ImageContainer>
+          ))}
+        </ImageContainer>
 
-          <InputContainer>
-            <InputDiv isError={!!errors.content}>
-              <StyledTextarea
-                {...register("content", {
-                  maxLength: {
-                    value: 10000,
-                    message: "10,000자를 넘을 수 없습니다",
-                  },
-                })}
-                placeholder="내용을 입력해주세요."
-              />
-            </InputDiv>
-          </InputContainer>
-          <ErrorMessage>{errors.content?.message}</ErrorMessage>
-        </div>
+        <InputDiv isError={!!errors.content}>
+          <StyledTextarea
+            {...register("content", {
+              maxLength: {
+                value: 10000,
+                message: "10,000자를 넘을 수 없습니다",
+              },
+            })}
+            placeholder="내용을 입력해주세요."
+          />
+        </InputDiv>
+        <ErrorMessage>{errors.content?.message}</ErrorMessage>
+
         {!initForm ? (
           <ButtonContainer>
-            <ButtonImage onClick={onClickImageUpload} />
+            <ImageButtonLabel htmlFor="image">
+              <Icon mode="fas" icon="image" size="3rem" color="#8E8E8E" />
+            </ImageButtonLabel>
+            <DisplayNoneInput
+              type="file"
+              id="image"
+              accept="image/png, image/jpeg, image/bmp"
+              name="image"
+            />
             <ButtonBottom label="확인" type="submit"></ButtonBottom>
           </ButtonContainer>
         ) : (
           <ButtonContainer>
-            <ButtonImage onClick={onClickImageUpload} />
+            <ImageButtonLabel htmlFor="image">
+              <Icon mode="fas" icon="image" size="3rem" color="#8E8E8E" />
+            </ImageButtonLabel>
+            <DisplayNoneInput
+              onChange={handleImageUpload}
+              type="file"
+              id="image"
+              accept="image/png, image/jpeg, image/bmp"
+              name="image"
+            />
             <ButtonTrashCan onClick={onClickDeleteButton} />
             <ButtonBottom label="완료" type="submit" />
           </ButtonContainer>
