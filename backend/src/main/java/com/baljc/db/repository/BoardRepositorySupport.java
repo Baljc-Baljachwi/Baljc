5ccd9b7be94ddaf24d1dfc359b1734566c30cc31
김수연 <lovesu5632@naver.com>
2022-05-10 16:06:08 +0900

    Fix: 불필요 코드 주석 처리

--- backend/src/main/java/com/baljc/db/repository/BoardRepositorySupport.java
+++ backend/src/main/java/com/baljc/db/repository/BoardRepositorySupport.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static com.querydsl.core.types.ExpressionUtils.count;
+
 @RequiredArgsConstructor
 @Repository
 public class BoardRepositorySupport {
@@ -28,40 +30,41 @@ public class BoardRepositorySupport {
     QMember qMember = QMember.member;
     QMember qMemberSelf = QMember.member;
     QHeart qHeart = QHeart.heart;
+    QScrap qScrap = QScrap.scrap;
     QComment qComment = QComment.comment1;
     QBoardImg qBoardImg = QBoardImg.boardImg;
 
-    public Optional<List<BoardDto.BoardListDto>> getBoardList(UUID categoryId, Long index, Member member) {
-        NumberPath<Double> distance = Expressions.numberPath(Double.class, "distance");
-
-        List<BoardDto.BoardListDto> response = jpaQueryFactory.select(
-                new QBoardDto_BoardListDto(qBoard.boardId, qBoardCategory.name, qBoard.content, qBoard.createdAt, qMember.nickname, qBoard.dong, qHeart.count(), qComment.count(),
-                        (MathExpressions.acos(MathExpressions.cos(MathExpressions.radians(qMemberSelf.latitude)).multiply(MathExpressions.cos(MathExpressions.radians(qBoard.latitude))))
-                                .multiply(MathExpressions.cos(MathExpressions.radians(qBoard.longitude))).multiply(6371).subtract(MathExpressions.radians(qMemberSelf.longitude)))
-                                .add(MathExpressions.sin(MathExpressions.radians(qMemberSelf.latitude)).multiply(MathExpressions.sin(MathExpressions.radians(qBoard.latitude))))
-                ))
-                .from(qBoard)
-                .leftJoin(qBoardCategory).on(qBoard.boardCategory.eq(qBoardCategory))
-                .leftJoin(qMember).on(qBoard.member.eq(qMember))
-                .leftJoin(qHeart).on(qHeart.board.eq(qBoard))
-                .leftJoin(qComment).on(qComment.board.eq(qBoard))
-                .leftJoin(qMemberSelf)
-                .groupBy(qBoard)
-                .having(
-                        JPAExpressions
-                                .select((MathExpressions.acos(MathExpressions.cos(MathExpressions.radians(Expressions.constant(member.getLatitude()))).multiply(MathExpressions.cos(MathExpressions.radians(qBoard.latitude))))
-                                        .multiply(MathExpressions.cos(MathExpressions.radians(qBoard.longitude))).multiply(6371).subtract(MathExpressions.radians(Expressions.constant(member.getLongitude()))))
-                                        .add(MathExpressions.sin(MathExpressions.radians(Expressions.constant(member.getLatitude()))).multiply(MathExpressions.sin(MathExpressions.radians(qBoard.latitude)))))
-                                .from(qBoard)
-                                .lt(4.0))
-                .where(qBoardCategory.boardCategoryId.eq(categoryId), qBoard.deletedYn.eq('N'))
-                .orderBy(qBoard.createdAt.desc())
-                .offset(index)
-                .limit(20)
-                .fetch();
-
-        return Optional.ofNullable(response);
-    }
+//    public Optional<List<BoardDto.BoardListDto>> getBoardList(UUID categoryId, Long index, Member member) {
+//        NumberPath<Double> distance = Expressions.numberPath(Double.class, "distance");
+//
+//        List<BoardDto.BoardListDto> response = jpaQueryFactory.select(
+//                new QBoardDto_BoardListDto(qBoard.boardId, qBoardCategory.name, qBoard.content, qBoard.createdAt, qMember.nickname, qBoard.dong, qHeart.count(), qComment.count(),
+//                        (MathExpressions.acos(MathExpressions.cos(MathExpressions.radians(qMemberSelf.latitude)).multiply(MathExpressions.cos(MathExpressions.radians(qBoard.latitude))))
+//                                .multiply(MathExpressions.cos(MathExpressions.radians(qBoard.longitude))).multiply(6371).subtract(MathExpressions.radians(qMemberSelf.longitude)))
+//                                .add(MathExpressions.sin(MathExpressions.radians(qMemberSelf.latitude)).multiply(MathExpressions.sin(MathExpressions.radians(qBoard.latitude))))
+//                ))
+//                .from(qBoard)
+//                .leftJoin(qBoardCategory).on(qBoard.boardCategory.eq(qBoardCategory))
+//                .leftJoin(qMember).on(qBoard.member.eq(qMember))
+//                .leftJoin(qHeart).on(qHeart.board.eq(qBoard))
+//                .leftJoin(qComment).on(qComment.board.eq(qBoard))
+//                .leftJoin(qMemberSelf)
+//                .groupBy(qBoard)
+//                .having(
+//                        JPAExpressions
+//                                .select((MathExpressions.acos(MathExpressions.cos(MathExpressions.radians(Expressions.constant(member.getLatitude()))).multiply(MathExpressions.cos(MathExpressions.radians(qBoard.latitude))))
+//                                        .multiply(MathExpressions.cos(MathExpressions.radians(qBoard.longitude))).multiply(6371).subtract(MathExpressions.radians(Expressions.constant(member.getLongitude()))))
+//                                        .add(MathExpressions.sin(MathExpressions.radians(Expressions.constant(member.getLatitude()))).multiply(MathExpressions.sin(MathExpressions.radians(qBoard.latitude)))))
+//                                .from(qBoard)
+//                                .lt(4.0))
+//                .where(qBoardCategory.boardCategoryId.eq(categoryId), qBoard.deletedYn.eq('N'))
+//                .orderBy(qBoard.createdAt.desc())
+//                .offset(index)
+//                .limit(20)
+//                .fetch();
+//
+//        return Optional.ofNullable(response);
+//    }
 
     public List<String> getImgURLList(UUID boardId) {
         List<String> response = jpaQueryFactory.select(qBoardImg.imgUrl)
