0eee305f6026f57316778b25a5fb9b6d6bb921e0
한지희 <g2hhh2ee@gmail.com>
2022-05-10 14:53:05 +0900

    Feat: 마이페이지 메뉴 추가

--- frontend/components/mypage/ProfileContentList.tsx
+++ frontend/components/mypage/ProfileContentList.tsx
@@ -14,29 +14,29 @@ export default function ProfileContentList() {
   return (
     <>
       <PageContainer>
-        {/* <ProfileContentCard
-          title="내가 쓴 글"
-          description="내 생활 습관을 분석해보세요."
+        <ProfileContentCard
+          title='내가 쓴 글 모아보기'
+          description='내가 쓴 글을 모아보세요.'
+          onClick={() => router.push("/mypage/posts")}
+        />
+        <ProfileContentCard
+          title='좋아요 한 글 모아보기'
+          description='내가 스크랩한 글을 모아보세요.'
+          onClick={() => router.push("/mypage/liked")}
+        />
+        <ProfileContentCard
+          title='목표를 향해서!'
+          description='내 생활 습관을 분석해보세요.'
           onClick={() => router.push("/mypage/analysis")}
         />
         <ProfileContentCard
-          title="스크랩한 글"
-          description="내 생활 습관을 분석해보세요."
-          onClick={() => router.push("/mypage/analysis")}
-        /> */}
-        <ProfileContentCard
-          title="목표를 향해서!"
-          description="내 생활 습관을 분석해보세요."
-          onClick={() => router.push("/mypage/analysis")}
-        />
-        <ProfileContentCard
-          title="고정 지출 관리"
-          description="고정 지출을 등록해보세요."
+          title='고정 지출 관리'
+          description='고정 지출을 등록해보세요.'
           onClick={() => router.push("/mypage/fixed")}
         />
         <ProfileContentCard
-          title="설정"
-          description="내 입맛대로 설정을 해볼까요?"
+          title='설정'
+          description='푸쉬 알림을 설정할 수 있습니다.'
           onClick={() => router.push("/mypage/settings")}
         />
       </PageContainer>
