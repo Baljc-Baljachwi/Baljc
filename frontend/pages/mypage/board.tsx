68069a6fb8a835a0fc959407c557a8912dbf1ad2
한지희 <g2hhh2ee@gmail.com>
2022-05-11 23:18:56 +0900

    Feat: 글 목록 없을 때  보일 컴포넌트 추가

--- frontend/pages/mypage/board.tsx
+++ frontend/pages/mypage/board.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import styled from "styled-components";
-import { getBoardsCategories, getBoardsList } from "api/community";
-import { getMyBoardList, getScrapBoardList } from "api/mypage";
+// import { getBoardsCategories, getBoardsList } from "api/community";
+import { getMyBoardList, getMyScrapList } from "api/mypage";
 import { IPost, IMyBoard } from "types";
-
+import ButtonImage from "components/common/ButtonImage";
 import Header from "components/common/Header";
 import CommunityCard from "components/community/CommunityCard";
 
@@ -48,10 +48,41 @@ const Board = () => {
   // console.log(myBoardList);
   return (
     <>
-      <Header label="내가 쓴 글 목록"></Header>
+      <Header
+        label="내가 쓴 글 목록"
+        onClickBackButton={() => router.push("/mypage")}
+      ></Header>
       <Container>
         <BodyContainer>
-          {myBoardList?.map((myBoardList: any) => (
+          {myBoardList?.length !== 0 ? (
+            myBoardList?.map((myBoardList: any) => (
+              <CommunityCard
+                key={myBoardList.boardId}
+                boardId={myBoardList.boardId}
+                categoryName={myBoardList.categoryName}
+                content={myBoardList.content}
+                createdAt={myBoardList.createdAt}
+                creator={myBoardList.creator}
+                dong={myBoardList.dong}
+                imgUrlList={myBoardList.imgUrlList}
+                heartCnt={myBoardList.heartCnt}
+                commentCnt={myBoardList.commentCnt}
+              />
+            ))
+          ) : (
+            <NoContentContainer>
+              <NoContentMessage>
+                {/* <NotFoundTransaction /> */}
+                작성한 글이 없습니다!
+                <NoContentMessage className="small">
+                  커뮤니티에서 글을 써보세요!
+                  {/* <div className="btn_MoveTo">동네 소식 보러 가기</div> */}
+                </NoContentMessage>
+                {/* <ButtonImage label="동네 소식 보러 가기"></ButtonImage> */}
+              </NoContentMessage>
+            </NoContentContainer>
+          )}
+          {/* {myBoardList?.map((myBoardList: any) => (
             <CommunityCard
               key={myBoardList.boardId}
               boardId={myBoardList.boardId}
@@ -64,7 +95,7 @@ const Board = () => {
               heartCnt={myBoardList.heartCnt}
               commentCnt={myBoardList.commentCnt}
             />
-          ))}
+          ))} */}
         </BodyContainer>
       </Container>
     </>
@@ -113,4 +144,48 @@ const ChatButtonDiv = styled.div`
   position: fixed;
   bottom: 14rem;
   z-index: 1000;
+`;
+const NoContentContainer = styled.section`
+  width: 100%;
+  // height: calc(100vh - 50rem);
+  // height: 100%;
+  // height: 100vh;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NoContentMessage = styled.p`
+  text-align: center;
+  font-size: 2.4rem;
+  font-weight: 700;
+  padding: 1rem;
+  color: #cccccc;
+  &.small {
+    font-size: 1.8rem;
+    font-weight: 500;
+    .btn_MoveTo {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+`;
+
+const StyledButton = styled.button`
+  color: #ffffff;
+  padding: 1rem 1.2rem;
+  font-weight: 700;
+  font-family: "Noto Sans KR", sans-serif;
+  font-style: normal;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border: 1px solid #8e8e8e;
+  border-radius: 0.8rem;
+  background-color: #ffffff;
+  cursor: pointer;
 `;
