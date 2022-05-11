fa1b23ba8b2107160532918d9b3b2899a13b39c9
김수연 <lovesu5632@naver.com>
2022-05-11 10:49:23 +0900

    Fix: 게시글 목록 조회 API 쿼리 조회 시 nickname alias creator로 수정

--- backend/src/main/java/com/baljc/db/repository/BoardRepository.java
+++ backend/src/main/java/com/baljc/db/repository/BoardRepository.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 
 public interface BoardRepository extends JpaRepository<Board, UUID> {
 
-    @Query(value = "SELECT b.board_id as boardId, bc.name as categoryName, b.content as content, b.created_at as createdAt, m.nickname as nickname, b.dong as dong, count(h.heart_id) as heartCnt, count(c.comment_id) as commentCnt, " +
+    @Query(value = "SELECT b.board_id as boardId, bc.name as categoryName, b.content as content, b.created_at as createdAt, m.nickname as creator, b.dong as dong, count(h.heart_id) as heartCnt, count(c.comment_id) as commentCnt, " +
             "(6371*acos(cos(radians(:lat))*cos(radians(b.latitude))*cos(radians(b.longitude) " +
             "-radians(:lon))+sin(radians(:lat))*sin(radians(b.latitude)))) as distance " +
             "FROM board b " +
@@ -32,7 +32,7 @@ public interface BoardRepository extends JpaRepository<Board, UUID> {
             "LIMIT :idx, 20", nativeQuery = true)
     List<BoardDto.BoardListInterface> getBoardListAll(@Param(value = "lat") double lat, @Param(value = "lon") double lon, @Param(value = "idx") long idx);
 
-    @Query(value = "SELECT b.board_id as boardId, bc.name as categoryName, b.content as content, b.created_at as createdAt, m.nickname as nickname, b.dong as dong, count(h.heart_id) as heartCnt, count(c.comment_id) as commentCnt, " +
+    @Query(value = "SELECT b.board_id as boardId, bc.name as categoryName, b.content as content, b.created_at as createdAt, m.nickname as creator, b.dong as dong, count(h.heart_id) as heartCnt, count(c.comment_id) as commentCnt, " +
             "(6371*acos(cos(radians(:lat))*cos(radians(b.latitude))*cos(radians(b.longitude) " +
             "-radians(:lon))+sin(radians(:lat))*sin(radians(b.latitude)))) as distance " +
             "FROM board b " +
