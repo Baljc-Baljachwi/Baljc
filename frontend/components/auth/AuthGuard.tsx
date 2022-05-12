e7f750e3ba58cac5179b9ccbddc4cdc4a858a45f
이성재 <xpsxm468@naver.com>
2022-05-12 11:28:45 +0900

    Fix: 커뮤니티 이동 전에 regionYn 확인하기

--- frontend/components/auth/AuthGuard.tsx
+++ frontend/components/auth/AuthGuard.tsx
@@ -7,10 +7,19 @@ export function AuthGuard({ children }: { children: JSX.Element }) {
   const userInfo = useRecoilValue(userInfoState);
   const router = useRouter();
   useEffect(() => {
+    console.log(router.pathname.split("/")[1]);
     if (!userInfo || !userInfo.accessToken) {
       router.push("/");
     } else if (!userInfo.surveyedYn) {
       router.push("/auth/survey");
+    } else if (
+      router.pathname.split("/")[1] === "community" &&
+      !userInfo.regionYn
+    ) {
+      confirm(
+        "커뮤니티 이용을 위해서는 위치 정보가 필요합니다!\n마이페이지에서 위치 정보를 수정해주세요. "
+      );
+      router.back();
     }
   }, [userInfo, router]);
 
