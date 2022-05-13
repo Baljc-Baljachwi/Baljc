ccfb58dac1cefa2ac55bda087b9ae58856f699ee
박주미 <zummy@kakao.com>
2022-05-13 14:46:30 +0900

    Feat: google analytics 설정 추가

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,33 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
   return (
     <Html>
       <Head>
         <meta charSet="utf-8" />
-        {/* <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         />
-        <meta name="description" content="Dev.log" />
-        <meta
-          name="keywords"
-          content="blog,react,antd,webpack,css,javascript"
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+        });
+      `,
+          }}
         />
-        <link rel="manifest" href="/static/manifest.json" />
-        <link rel="shortcut icon" href="/static/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/earlyaccess/notosanskr.css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-          rel="stylesheet"
-        /> */}
       </Head>
       <body>
         <Main />
