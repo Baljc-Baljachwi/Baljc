10dd986115871ba0085972ec7d70a63d718f62a0
김도현 <kdh494849@gmail.com>
2022-05-16 17:38:53 +0900

    Feat: 채팅 서버 세팅 및 배포

---
+++ chat/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
+  await app.listen(port);
+}
+bootstrap();
